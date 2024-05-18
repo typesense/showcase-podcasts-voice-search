c406649f463b30f95f6baf86839d7f821be78d40
Hayden <hayden.hhoang@gmail.com>
2024-05-18 21:30:45 +0700

    use node url in typesense config

--- src/lib/typesense.ts
+++ src/lib/typesense.ts
@@ -19,9 +19,7 @@ export const typesense = new Typesense.Client({
   apiKey: import.meta.env.VITE_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
   nodes: [
     {
-      host: import.meta.env.VITE_TYPESENSE_HOST || 'localhost',
-      port: parseInt(import.meta.env.VITE_TYPESENSE_PORT || '8108'),
-      protocol: import.meta.env.VITE_TYPESENSE_PROTOCOL || 'http',
+      url: import.meta.env.VITE_TYPESENSE_URL || 'http://localhost:8108',
     },
   ],
   connectionTimeoutSeconds: 60 * 60,
