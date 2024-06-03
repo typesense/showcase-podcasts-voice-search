f0f2a68aee2cd00849f8e57ec87434cffbf1397d
Hayden <hayden.hhoang@gmail.com>
2024-06-03 17:24:22 +0700

    🎨: app top padding

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   }, []);
 
   return (
-    <main className='max-w-3xl max-md:px-2 m-auto pt-10 pb-20 flex flex-col gap-8 items-center'>
+    <main className='max-w-3xl max-md:px-2 m-auto pt-6 pb-20 flex flex-col gap-8 items-center'>
       <Header />
       <div className='w-full flex flex-col gap-6'>
         <Heading />
@@ -56,7 +56,7 @@ function App() {
   );
 }
 const Header = () => (
-  <header className=' w-full flex justify-end mb-[-2rem]'>
+  <header className=' w-full flex justify-end mb-[-1.5rem]'>
     <a
       href='https://github.com/typesense/showcase-podcasts-voice-search'
       target='_blank'
